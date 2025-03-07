b88f08833e35658403197558a77a2385c9ebb200
Ciprian-Valentin NEAMU <ciprian.neamu@stud.acs.upb.ro>
2025-03-07 16:23:25 +0000

    feat: memory & commit modules

--- src/checker-modules/registered.go
+++ src/checker-modules/registered.go
@@ -2,7 +2,7 @@ package checker_modules
 
 var AvailableModules = map[string]CheckerModule{
 	"ref_checker":    NewDummyModule(),
-	"memory_checker": NewDummyModule(),
+	"memory_checker": &MemoryChecker{},
 	"style_checker":  &StyleChecker{},
-	"commit_checker": NewDummyModule(),
+	"commit_checker": &CommitChecker{},
 }
