554c997e050b7f01e5fe45a2bf1bfd797a8c151f
Asavoae Cosmin-Stefan <cosmin.asavoae@stud.acs.upb.ro>
2025-03-12 01:19:52 +0200

    config: added base macros
    Signed-off-by: Asavoae Cosmin-Stefan <cosmin.asavoae@stud.acs.upb.ro>

--- src/utils/config.go
+++ src/utils/config.go
@@ -2,8 +2,10 @@ package utils
 
 import (
 	"encoding/json"
+	"fmt"
 	"log/slog"
 	"os"
+	"strings"
 )
 
 const (
@@ -65,4 +67,20 @@ func SaveUserConfig() {
 
 func Log(str string) {
 	logger.Info(str)
+}
+
+var ConfigMacros = make(map[string]string)
+
+func ConvertMacros(srcStr string, contextMacros map[string]string) string {
+	// Replace implicit macros
+	for k, v := range ConfigMacros {
+		srcStr = strings.ReplaceAll(srcStr, fmt.Sprintf("$%s", k), v)
+	}
+
+	// Replace context macros
+	for k, v := range contextMacros {
+		srcStr = strings.ReplaceAll(srcStr, fmt.Sprintf("$%s", k), v)
+	}
+
+	return srcStr
 }
